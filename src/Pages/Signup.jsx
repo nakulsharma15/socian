bf34957bcd3ff89fd68ddafb63b01eb93bafec9e
nakulsharma15 <nakuls309@gmail.com>
2022-07-14 12:40:29 +0530

    Update Signup.jsx

--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -13,7 +13,7 @@ export const Signup = () => {
             <img src="https://res.cloudinary.com/nakulsharma15/image/upload/v1657730491/Assets/02012019-20-removebg-preview_gqjanh_dtas4n.png" alt="pp-logo" />
           </div>
           
-          <p className='auth-page-brand-text'>Socian helps you connect and share with the people in your life Signup.</p>
+          <p className='auth-page-brand-text'>Socian helps you connect and share with the people in your life.</p>
         </div>
 
         <SignupForm />
