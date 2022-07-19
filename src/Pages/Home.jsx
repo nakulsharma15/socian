13b8a58b651c05ad485727be48676c58ab1e4fc2
nakulsharma15 <nakuls309@gmail.com>
2022-07-19 17:16:34 +0530

    feat: added post sorting on home feed

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,21 +3,26 @@ import "./Styles/Styles.css";
 import { SideNav, CreatePost, Header, SuggestionBar, PostCard, EditPostModal } from '../Components/index';
 import { useDispatch, useSelector } from "react-redux";
 import { getAllPosts } from '../utils/postHandler';
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { getAllUsers } from '../utils/userHandler';
 
 export const Home = () => {
 
   const dispatch = useDispatch();
+  const [sortType, setSortType] = useState("Recent");
   const { postList } = useSelector((store) => store.posts);
-  const {userData} = useSelector((store) => store.auth);
+  const { userData } = useSelector((store) => store.auth);
   const { isEditPostModalOpen } = useSelector((store) => store.modal);
 
   const relevantPosts = postList?.filter((post) => {
     const temp = userData.following.find((follower) => follower.username === post.username);
-    if(temp || post.username === userData.username)
-    return post;
+    if (temp || post.username === userData.username)
+      return post;
   });
+
+  const sortHandler = (type) => {
+    setSortType(type);
+  }
 
   useEffect(() => {
     dispatch(getAllUsers());
@@ -40,7 +45,20 @@ export const Home = () => {
 
           <CreatePost />
 
-          {relevantPosts?.map((post) => <PostCard post={post} key={post._id} />)}
+          <div className="home-feed-sort-div">
+
+            <div className={`home-feed-sort-div-component ${sortType === "Recent" ? "sort-active" : null}`} onClick={() => sortHandler("Recent")}>
+              <p>Recent First</p>
+            </div>
+
+            <div  className={`home-feed-sort-div-component ${sortType === "Older" ? "sort-active" : null}`} onClick={() => sortHandler("Older")}>
+              <p>Older First</p>
+            </div>
+          </div>
+
+          <div className={sortType === "Recent" ? "newest-first-class" : "older-first-class"}>
+            {relevantPosts?.map((post) => <PostCard post={post} key={post._id} />)}
+          </div>
 
         </div>
 
